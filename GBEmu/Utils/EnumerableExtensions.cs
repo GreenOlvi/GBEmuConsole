8b112a1065d1a8d62edbcc5a87bed5c375aa786f
Piotr Szulc <szulo123@gmail.com>
2019-10-03 22:18:48 +0200

    Added tests for Chunkify

--- GBEmu/Utils/EnumerableExtensions.cs
+++ GBEmu/Utils/EnumerableExtensions.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace GBEmu.Utils
 {
     public static class EnumerableExtensions
     {
-        public static IEnumerable<IEnumerable<T>> Chunkify<T>(this IEnumerable<T> input, int chunkSize) =>
-            input.Select((e, i) => (e, i))
+        public static IEnumerable<IEnumerable<T>> Chunkify<T>(this IEnumerable<T> input, int chunkSize)
+        {
+            if (chunkSize <= 0)
+            {
+                throw new ArgumentException("Chunk size should be larger than 0");
+            }
+
+            return input.Select((e, i) => (e, i))
                 .GroupBy(t => t.i / chunkSize)
                 .Select(g => g.Select(t => t.e));
+
+        }
     }
 }
---
+++ GBTests/Utils/EnumerableExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using FluentAssertions;
+using GBEmu.Utils;
+using System;
+
+namespace GBTests.Utils
+{
+    [TestFixture]
+    public class EnumerableExtensionsTests
+    {
+        private static IEnumerable<TestCaseData> ChunkifyTestCases()
+        {
+            yield return new TestCaseData(new byte[0], 1, new byte[0][]);
+            yield return new TestCaseData(new byte[] { 0xde, 0xad, 0xbe, 0xef }, 2,
+                new byte[][] { new byte[] { 0xde, 0xad }, new byte[] { 0xbe, 0xef } });
+            yield return new TestCaseData(new byte[] { 1, 2, 3, 4 }, 3,
+                new byte[][] { new byte[] { 1, 2, 3 }, new byte[] { 4 } });
+        }
+
+        [Test]
+        [TestCaseSource(nameof(ChunkifyTestCases))]
+        public void ChunkifyTests(byte[] input, int chunkSize, byte[][] expected)
+        {
+            input.Chunkify(chunkSize).Should().BeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void ChunkifyShouldThrowWhenGivenNullTest()
+        {
+            IEnumerable<byte> input = null;
+            Action a = () => input.Chunkify(2);
+            a.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ChunkifyShouldThrowWhenGivenNegativeSizeTest()
+        {
+            Action a = () => new string[] { "a", "b", "c", "d" }.Chunkify(-2);
+            a.Should().ThrowExactly<ArgumentException>();
+        }
+    }
+}
